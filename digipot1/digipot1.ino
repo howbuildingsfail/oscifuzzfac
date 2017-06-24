5d48945bd16dd2ad522f94a1a2acaddaf2a375e7
franticspider <sjh518@york.ac.uk>
2017-06-24 13:53:54 +0100

    Working midi for four channels: dist comp gate stab

--- digipot1/digipot1.ino
+++ digipot1/digipot1.ino
@@ -1,77 +1,206 @@
-int CS_signal = 5;//2;                      // Chip Select signal onsul pin 2 of Arduino
-int CLK_signal = 6;//4;                     // Clock signal on pin 4 of Arduino
-int MOSI_signal = 7;//5;                    // MOSI signal on pin 5 of Arduino
+/*********************************************************************************/
+/*  http://sensorium.github.io/Mozzi/learn/a-simple-sketch/  *********************/
+/*********************************************************************************/
 
+#include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
+#include <tables/saw2048_int8.h>
+
+#define CONTROL_RATE (128)
+
+Oscil <2048, CONTROL_RATE> compSin(SIN2048_DATA);
+//Sketch uses 10,100 bytes (32%) of program storage space. Maximum is 30,720 bytes.
+//Global variables use 1,009 bytes (49%) of dynamic memory, leaving 1,039 bytes for local variables. Maximum is 2,048 bytes.
+
+Oscil <2048, CONTROL_RATE> gateSin(SIN2048_DATA);
+Oscil <2048, CONTROL_RATE> distSin(SIN2048_DATA);
+Oscil <2048, CONTROL_RATE> stabSin(SIN2048_DATA);
+
+
+float depth=0.25;
+
+
+/*********************************************************************************/
+/* MIDI - from jamjar drumkit ****************************************************/
+/*********************************************************************************/
+
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+#include <mozzi_midi.h>
+
+
+void HandleNoteOn(byte channel, byte note, byte velocity) {
+  
+  switch (note) {
+    case 24:
+      break;
+  }
+}
+
+
+
+void HandleNoteOff(byte channel, byte note, byte velocity) {
+  
+  switch (note) {
+    case 24:
+      break;
+  }
+}
+
+
+void HandleControlChange (byte channel, byte number, byte value){
+  switch (number){
+    /***************************************************************/
+    case 16:
+      compSin.setFreq((float) 0.5 * value);    
+      break;
+
+    case 17:
+      compSin.setFreq((float) 0.05 * value);    
+      break;
+
+      
+    /***************************************************************/
+    case 32:
+      gateSin.setFreq((float) 0.5 * value);    
+      break;
+      
+    case 33:
+      gateSin.setFreq((float) 0.05 * value);    
+      break;
+
+    
+  }
+  
+}
+
+/*********************************************************************************/
+
+//TODO: we could #define these!
+byte aCS_signal = 5;//2;                      // Chip Select signal on pin 5 of Arduino
+byte aCLK_signal = 6;//4;                     // Clock signal on pin 6 of Arduino
+byte aMOSI_signal = 7;//5;                    // MOSI signal on pin 7 of Arduino
+
+
+byte bCS_signal = 2;                      // Chip Select signal on pin 5 of Arduino
+byte bCLK_signal = 3;                     // Clock signal on pin 6 of Arduino
+byte bMOSI_signal = 4;                    // MOSI signal on pin 7 of Arduino
+
+#define LED_gate 10     //output flashing LED in time with the LFO rate for GATE pot
+
+
+
+
+//TODO: we could #define these!
 byte cmd_byte1    = B00010010 ;            // Command byte
 byte cmd_byte0    = B00010001 ;            // Command byte
 byte cmd_byteboth = B00010011 ;            // Command byte
 
 int initial_value = 100;                // Setting up the initial value
 
-int biff = B11;
 
-void initialize() {                     // send the command byte of value 100 (initial value)
-    spi_out(CS_signal, cmd_byteboth, initial_value);
+void initialize_spi(byte mosi, byte clk) {                     // send the command byte of value 100 (initial value)
+    //TODO: need to initialise all four pots - 
+    spi_out(aCS_signal, cmd_byteboth, initial_value, mosi, clk);
 }
 
-void spi_out(int CS, byte cmd_byte, byte data_byte){                        // we need this function to send command byte and data byte to the chip
+void spi_out(int CS, byte cmd_byte, byte data_byte, byte mosi, byte clk){                        // we need this function to send command byte and data byte to the chip
     
     digitalWrite (CS, LOW);                                                 // to start the transmission, the chip select must be low
-    spi_transfer(cmd_byte); // invio il COMMAND BYTE
+    spi_transfer(cmd_byte,mosi,clk); // invio il COMMAND BYTE
     //delay(2);
-    spi_transfer(data_byte); // invio il DATA BYTE
+    spi_transfer(data_byte,mosi,clk); // invio il DATA BYTE
     //delay(2);
     digitalWrite(CS, HIGH);                                                 // to stop the transmission, the chip select must be high
 }
 
-void spi_transfer(byte working) {
-    for(int i = 1; i <= 8; i++) {                                           // Set up a loop of 8 iterations (8 bits in a byte)
-     if (working > 127) { 
-       digitalWrite (MOSI_signal,HIGH) ;                                    // If the MSB is a 1 then set MOSI high
-     } else { 
-       digitalWrite (MOSI_signal, LOW) ; }                                  // If the MSB is a 0 then set MOSI low                                           
-    
-    digitalWrite (CLK_signal,HIGH) ;                                        // Pulse the CLK_signal high
+void spi_transfer(byte working, byte mosi, byte clk) {
+  for(int i = 1; i <= 8; i++) {                                           // Set up a loop of 8 iterations (8 bits in a byte)
+    if (working > 127) { 
+      digitalWrite (mosi,HIGH) ;                                    // If the MSB is a 1 then set MOSI high
+    } 
+    else { 
+      digitalWrite (mosi, LOW) ;                                     // If the MSB is a 0 then set MOSI low     
+    }                                                                         
+    digitalWrite (clk,HIGH) ;                                        // Pulse the CLK_signal high
     working = working << 1 ;                                                // Bit-shift the working byte
-    digitalWrite(CLK_signal,LOW) ;                                          // Pulse the CLK_signal low
-    }
+    digitalWrite(clk,LOW) ;                                          // Pulse the CLK_signal low
+  }
 }
 
 void setup() {
-    pinMode (CS_signal, OUTPUT);
-    pinMode (CLK_signal, OUTPUT);
-    pinMode (MOSI_signal, OUTPUT);
+  pinMode (aCS_signal, OUTPUT);
+  pinMode (aCLK_signal, OUTPUT);
+  pinMode (aMOSI_signal, OUTPUT);
+  
+  pinMode (bCS_signal, OUTPUT);
+  pinMode (bCLK_signal, OUTPUT);
+  pinMode (bMOSI_signal, OUTPUT);
 
-    initialize();
+  pinMode (LED_gate, OUTPUT);
 
-    Serial.begin(9600);                                                     // setting the serial speed
-    Serial.println("ready!");
+  //TODO: Find good initial settings! - OR better still, send the midi control message for the default setting
+  initialize_spi(aMOSI_signal,aCLK_signal);
+
+
+  /* Freq: 6.5 is twice the speed of 3.5 */
+  compSin.setFreq(0.42f);
+  distSin.setFreq(0.42f);
+  stabSin.setFreq(0.42f);
+  gateSin.setFreq(0.42f);
+
+
+  // Initiate MIDI communications, listen to all channels
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(38400);/* This baud rate is recommended for ALSA: http://alsa.opensrc.org/Serial  */
+
+  // Connect the HandleNoteOn function to the library, so it is called upon reception of a NoteOn.
+  MIDI.setHandleNoteOn(HandleNoteOn);  // Put only the name of the function
+  MIDI.setHandleNoteOff(HandleNoteOff);  // Put only the name of the function
+  MIDI.setHandleControlChange(HandleControlChange);
+  
+  startMozzi(CONTROL_RATE);
 }
 
-void loop() {
 
-/*
-    for(int j=0;j<10;j++){
-    for (int i = 0; i < 255; i++) {
-        spi_out(CS_signal, cmd_byte0, i); 
-        //Serial.println(i); 
-        //delay(10); 
-    }
-    }
-    delay(200);
-    for(int j=0;j<10;j++){
-    for (int i = 0; i < 255; i++) {
-        spi_out(CS_signal, cmd_byte1, i); 
-        //Serial.println(i); 
-        //delay(10); 
-    }
-    }
-    delay(100);*/
-    for(int j=0;j<10;j++){
-    for (int i = 0; i < 255; i++) {
-        spi_out(CS_signal, cmd_byteboth, i); 
-        //Serial.println(i); 
-        //delay(10); 
-    }
-    }
+int lfo1,lfo2,lfo3,lfo4;
+
+void updateControl(){
+
+
+  MIDI.read();
+
+  //TODO: The following should be in the MIDI control commands
+  // compSin.next() returns a signed byte between -128 to 127 from the wave table
+  lfo1 = 128 + (0.9 * compSin.next());
+  lfo2 = 128 + (0.9 * gateSin.next());
+  lfo3 = 128 + (0.9 * distSin.next());
+  lfo4 = 128 + (0.9 * stabSin.next());
+
+
+  //spi_out(CS_signal, cmd_byteboth, lfo1); 
+  spi_out(aCS_signal, cmd_byte0, lfo1,  aMOSI_signal, aCLK_signal); 
+  spi_out(aCS_signal, cmd_byte1, lfo2,  aMOSI_signal, aCLK_signal); 
+  spi_out(bCS_signal, cmd_byte0, lfo3,  bMOSI_signal, bCLK_signal); 
+  spi_out(bCS_signal, cmd_byte1, lfo4,  bMOSI_signal, bCLK_signal); 
+
+  analogWrite(LED_gate, lfo2);
+  
+}
+
+
+int updateAudio(){
+ 
+  
+  return 1;
+}
+
+
+
+void loop() {
+  audioHook();
+  
 }
